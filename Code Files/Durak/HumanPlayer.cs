980de7b9ee111ed0df2ee03ba73930be5b90c444
LukasCadman <lukas.cadman@dcmail.ca>
2026-03-25 17:31:00 -0400

    Add .gitignore and remove .vs folder from tracking

--- Code Files/Durak/HumanPlayer.cs
+++ Code Files/Durak/HumanPlayer.cs
@@ -2,7 +2,20 @@
 
 public class HumanPlayer
 {
-    public HumanPlayer()
+
+    // Player Name
+    public string Name { get; set; }
+
+    // Cards in the Player's Hand
+    public List<Card> Hand { get; set; }
+
+    // Detect if the player is currently attacking
+    public bool IsAttacking { get; set; }
+
+    public HumanPlayer(string name)
     {
+        Name = name;
+        Hand = new List<Card>();
+
     }
 }
