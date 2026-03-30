1e5284671a1f7d74765666b39d29a97309c56921
MFlores43DC <matthew.flores@dcmail.ca>
2026-03-30 04:31:26 -0400

    Updated WPF UI, added Rules and Settings page with minimal functionality

--- Code Files/Durak/MainWindow.xaml.cs
+++ Code Files/Durak/MainWindow.xaml.cs
@@ -21,6 +21,18 @@ namespace Durak
             InitializeComponent();
         }
 
+        private void Main_RulesButton_Click(object sender, RoutedEventArgs e)
+        {
+            GameRules rulesWindow = new GameRules();
+            rulesWindow.Owner = this;
+            rulesWindow.ShowDialog();
+        }
 
+        private void Main_StatsSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            StatsSettings statSetWindow = new StatsSettings();
+            statSetWindow.Owner = this;
+            statSetWindow.ShowDialog();
+        }
     }
 }
---
+++ Code Files/Durak/StatsSettings.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Durak
+{
+    /// <summary>
+    /// Interaction logic for StatsSettings.xaml
+    /// </summary>
+    public partial class StatsSettings : Window
+    {
+        public StatsSettings()
+        {
+            InitializeComponent();
+        }
+
+        private void StatSet_SaveAndExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void StatSet_ExitWithoutSavingButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void StatSet_NormalThemeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StatSet_InvertedThemeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StatSet_SillyThemeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
